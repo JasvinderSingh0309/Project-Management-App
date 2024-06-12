af6034eb275c7c51fc6639bdc52304d462f10af3
Jasvinder Singh <jasvinder4251@gmail.com>
2024-06-12 13:54:56 +0530

    to add new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Project from "./components/Project";
+import NewProject from "./components/NewProject";
 
 function App() {
   return (
     <>
       <Sidebar />
-      <Project />
+      {/* <Project /> */}
+      <NewProject />
     </>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+export default function Input({title, type}) {
+
+  let cls = "w-full p-1 rounded-sm border-stone-300 bg-stone-200 text-stone-600 outline-none focus:border-stone-600";
+
+  let txtArea = false;
+  if(!type) {
+    txtArea = true;
+  }
+
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">{title}</label>
+      {txtArea? <textarea rows={5} className={cls}></textarea> : <input className={cls} type={type} />}
+    </p>
+  )
+}
