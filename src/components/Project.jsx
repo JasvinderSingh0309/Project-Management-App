aa24ca857be8d6a6796e601d5d79d489267c9b5e
Jasvinder Singh <jasvinder4251@gmail.com>
2024-06-12 12:43:10 +0530

    change some style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ export default function Project() {
       <img src="./src/assets/no-projects.png" alt="no project logo" className="w-20"/>
       <h2 className=" text-amber-800 text-2xl font-bold mt-2">No Project Selected</h2>
       <p className="my-7 text-gray-500 text-lg">Select a project or get started with a new one</p>
-      <button className="px-3 py-2 rounded-md bg-slate-800 text-slate-300">Create new project</button>
+      <button className="px-3 py-2 rounded-md bg-stone-500 text-stone-300  hover:bg-stone-600">Create new project</button>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 export default function Sidebar() {
   return (
     <nav className="w-1/3 bg-black max-w-80 p-10 rounded-tr-2xl">
-      <h2 className="uppercase text-white text-2xl mb-4 font-semibold">Your Projects</h2>
-      <button className="px-3 py-2 bg-slate-800 text-slate-500 rounded-md">+ Add Project</button>
-      <ul></ul>
+      <h2 className="uppercase text-white md:text-2xl mb-4 font-semibold">Your Projects</h2>
+      <button className="px-3 py-2 bg-stone-500 text-stone-300 rounded-md text-xs md:text-base hover:bg-stone-600">+ Add Project</button>
+      <ul className="text-white py-8 text-xl">
+        <li>{}</li>
+      </ul>
     </nav>
   );
 }
