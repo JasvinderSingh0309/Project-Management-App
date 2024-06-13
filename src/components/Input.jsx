b364feb1dda9b34059cacb2ccaf8328c6defc37b
Jasvinder Singh <jasvinder4251@gmail.com>
2024-06-13 13:36:30 +0530

    added refs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({title, type}) {
+import { forwardRef } from "react";
+
+let Input = forwardRef(function Input({title, type}, ref) {
 
   let cls = "w-full p-1 rounded-sm border border-stone-300 bg-stone-200 text-stone-600 outline-none focus:border-stone-800";
 
@@ -10,7 +12,9 @@ export default function Input({title, type}) {
   return (
     <p className="flex flex-col gap-1 my-4">
       <label className="text-sm font-bold uppercase text-stone-500">{title}</label>
-      {txtArea? <textarea rows={5} className={cls}></textarea> : <input className={cls} type={type} />}
+      {txtArea? <textarea ref={ref} rows={5} className={cls}></textarea> : <input className={cls} type={type} ref={ref} />}
     </p>
   )
-}
+});
+
+export default Input;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,27 @@
 import Input from "./Input";
+import { useRef } from "react";
 
 export default function NewProject() {
+  let title = useRef();
+  let des = useRef();
+  let date = useRef();
+
+  function handleInputValues() {
+    console.log(title.current.value);
+    console.log(des.current.value);
+    console.log(date.current.value);
+  }
+
   return (
     <div className="w-[35rem] mt-10">
       <menu className="flex items-center justify-end gap-4 my-4">
         <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+        <button onClick={handleInputValues} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
       </menu>
       <div>
-        <Input title="Title" type="text"/>
-        <Input title="Description" />
-        <Input title="Date" type="date"/>
+        <Input ref={title} title="Title" type="text"/>
+        <Input ref={des} title="Description" />
+        <Input ref={date} title="Date" type="date"/>
       </div>
     </div>
   );
