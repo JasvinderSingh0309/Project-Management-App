961c6abb6d211dd8e2ab6f02922c4bd55e578eb1
Jasvinder Singh <jasvinder4251@gmail.com>
2024-06-21 12:10:35 +0530

    todo works, but same for all

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,21 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export default function Todo() {
+  // So, it works well now. But we need to manage todos according to projects being selected, now all projects have the same todos.
   
-  //main issue here was, how to clear input field and add the task properly. 
-
+  const [value,  setValue] = useState("");
   const [todos,  setTodos] = useState([]);
-  let inputValue = useRef();
-  
-  function handleAddTasks() {
-    if(inputValue.current.value) {
-      setTodos(prev => [...prev, inputValue.current.value]);
-    }
-    inputValue.current.value = "";
+
+  function handleChange(event) {
+    setValue(event.target.value);
   }
 
-  // this is where we sjould consider using the older way to update state by just using useState(), instead of a ref.
+  function handleAddTodos() {
+    if(value) {
+      setTodos(prev => [...prev, value]);
+    }
+    setValue("");
+  }
 
   function clearTodo(index) {
     setTodos(prev => prev.filter((e,i) => i !== index));
@@ -26,8 +27,8 @@ export default function Todo() {
     <div>
       <h1 className="text-3xl font-bold py-4">Tasks</h1>
       <div>
-        <input ref={inputValue} type="text" className=" outline-none bg-gray-300 p-2 w-1/2 text-xl rounded focus:border focus:border-black"/>
-        <button className="ml-3" onClick={handleAddTasks}>Add task</button>
+        <input type="text" className=" outline-none bg-gray-300 p-2 w-1/2 text-xl rounded focus:border focus:border-black" onChange={handleChange} value={value} />
+        <button className="ml-3" onClick={handleAddTodos}>Add task</button>
       </div>
       {!todos.length? <p className="text-lg mt-8">This project does not have any tasks yet.</p> : 
         <ul className="text-lg mt-8 bg-gray-200/60 px-5 py-7 rounded">{todos.map((e,i) => <li key={i}  className="flex justify-between items-center py-2 text-xl">{e} <button className=" text-base" onClick={() => clearTodo(i)}>Clear</button></li>)}</ul>
