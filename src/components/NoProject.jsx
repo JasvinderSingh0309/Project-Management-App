81974f4eb04f422a350f4bd7de84276926f78467
Jasvinder Singh <jasvinder4251@gmail.com>
2024-06-22 12:02:20 +0530

    correct it again

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,7 +1,7 @@
 export default function NoProject({onAdd}) {
   return (
     <div className="flex-1 flex flex-col items-center pt-20">
-      <img src="../public/logo.png" alt="no project logo" className="w-20"/> {/* className="w-20 h-20 object-contain mx-auto (for centering)" */}
+      <img src="./public/logo.png" alt="no project logo" className="w-20"/> {/* className="w-20 h-20 object-contain mx-auto (for centering)" */}
       <h2 className="text-amber-800 text-2xl font-bold mt-2">No Project Selected</h2>
       <p className="my-7 text-gray-500 text-lg">Select a project or get started with a new one</p>
       <button onClick={onAdd} className="px-3 py-2 rounded-md bg-stone-500 text-stone-300  hover:bg-stone-600">Create new project</button>
